fd6a070c614115f9d802f2c0bc8f032171d22310
Pocky <pocky1559@proton.me>
2025-02-16 15:02:50 +0700

    add new console.log for print the final color

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const greenColor = chalk.green;
 const blueColor = chalk.blue;
 
 function calculateRGB(number) {
-  return Math.round((number / 255) / 100);
+  return (number / 255).toFixed(3);
 }
 
 console.log(`s${redColor('R')}${greenColor('G')}${blueColor('B')}Normalizer`);
@@ -15,10 +15,10 @@ console.log(`Please insert your color
 for exmaple: "255, 255, 255"`)
 let rgbColor = prompt('sRGB color: ');
 
-const [r, g, b] = rgbColor.split(',').map(Number);
+const [r255, g255, b255] = rgbColor.split(',').map(Number);
 
-r = calculateRGB(r);
-g = calculateRGB(g);
-b = calculateRGB(b);
+const r1 = calculateRGB(r255);
+const g1 = calculateRGB(g255);
+const b1 = calculateRGB(b255);
 
-console.log(`Here is your new sRGB number`)
+console.log(`Here is your new sRGB number ${redColor(r1)},${greenColor(g1)},${blueColor(b1)}`);
