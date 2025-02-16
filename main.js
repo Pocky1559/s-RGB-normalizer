bf8668f3ef65ee70dcb0a0d947bf28b11b8d29b6
Pocky <pocky1559@proton.me>
2025-02-16 17:28:22 +0700

    add function for continue using program or exit program

--- main.js
+++ main.js
@@ -1,32 +1,48 @@
 import chalk from 'chalk';
 import promptSync from 'prompt-sync';
-import clipboard from 'clipboardy'
+import clipboard from 'clipboardy';
 
 const prompt = promptSync();
 const redColor = chalk.red;
 const greenColor = chalk.green;
 const blueColor = chalk.blue;
+let confirm_loop;
 
 function calculateRGB(number) {
   return (number / 255).toFixed(3);
 }
 
-console.log(`s${redColor('R')}${greenColor('G')}${blueColor('B')}Normalizer`);
+function continueUsingProgram() {
+  while (true) {
+    confirm_loop = prompt('Do you want to continue using this program? (Y/N): ')
 
-console.log(`
-Please insert your color
-for exmaple: "${redColor(255)},${greenColor(255)},${blueColor(255)}"`)
-let rgbColor = prompt('sRGB color: ');
+    if (confirm_loop.toUpperCase() === 'Y') {
+      break;
+    } else if (confirm_loop.toUpperCase() === 'N') {
+      process.exit(0);
+    } else {
+      console.log('please type Y or N')
+    }
+  }
+}
 
-const [r255, g255, b255] = rgbColor.split(',').map(Number);
+while (true) {
+  console.clear();
+  console.log(`s${redColor('R')}${greenColor('G')}${blueColor('B')}Normalizer\nversion 1.1`);
 
-const r1 = calculateRGB(r255);
-const g1 = calculateRGB(g255);
-const b1 = calculateRGB(b255);
+  console.log(`\nPlease insert your color\nfor exmaple: "${redColor(255)},${greenColor(255)},${blueColor(255)}"`)
+  let rgbColor = prompt('sRGB color: ');
 
-console.log(`
-Here is your new sRGB number ${redColor(r1)},${greenColor(g1)},${blueColor(b1)}`);
+  const [r255, g255, b255] = rgbColor.split(',').map(Number);
 
-clipboard.writeSync(`${r1},${g1},${b1}`);
-console.log(`
-new sRGB number had copy to your clipboard`);
+  const r1 = calculateRGB(r255);
+  const g1 = calculateRGB(g255);
+  const b1 = calculateRGB(b255);
+
+  console.log(`\nHere is your new sRGB number ${redColor(r1)},${greenColor(g1)},${blueColor(b1)}`);
+
+  clipboard.writeSync(`${r1},${g1},${b1}`);
+  console.log(`\nnew sRGB number had copy to your clipboard\n`);
+
+  continueUsingProgram()
+}
