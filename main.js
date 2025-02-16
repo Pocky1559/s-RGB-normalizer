7a73c578123d2898984cc64309a75a4e73254efa
pocky <pissawat2551@gmail.com>
2025-02-16 14:29:05 +0700

    add new input for input the color

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('This is a test message')
+import chalk from 'chalk';
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+const redColor = chalk.red;
+const greenColor = chalk.green;
+const blueColor = chalk.blue;
+
+console.log(`s${redColor('R')}${greenColor('G')}${blueColor('B')}Normalizer`);
+
+console.log(`Please insert your color
+for exmaple: "255, 255, 255"`)
+let rgbColor = prompt('sRGB color: ');
