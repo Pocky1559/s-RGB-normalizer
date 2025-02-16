1171b8dd1db68bf033ac1f997ba9b7c38f441b17
Pocky <pocky1559@proton.me>
2025-02-16 15:22:45 +0700

    add automatically copy new sRGB number to clipboard

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import chalk from 'chalk';
 import promptSync from 'prompt-sync';
+import clipboard from 'clipboardy'
+
 const prompt = promptSync();
 const redColor = chalk.red;
 const greenColor = chalk.green;
@@ -11,8 +13,9 @@ function calculateRGB(number) {
 
 console.log(`s${redColor('R')}${greenColor('G')}${blueColor('B')}Normalizer`);
 
-console.log(`Please insert your color
-for exmaple: "255, 255, 255"`)
+console.log(`
+Please insert your color
+for exmaple: "${redColor(255)},${greenColor(255)},${blueColor(255)}"`)
 let rgbColor = prompt('sRGB color: ');
 
 const [r255, g255, b255] = rgbColor.split(',').map(Number);
@@ -21,4 +24,9 @@ const r1 = calculateRGB(r255);
 const g1 = calculateRGB(g255);
 const b1 = calculateRGB(b255);
 
-console.log(`Here is your new sRGB number ${redColor(r1)},${greenColor(g1)},${blueColor(b1)}`);
+console.log(`
+Here is your new sRGB number ${redColor(r1)},${greenColor(g1)},${blueColor(b1)}`);
+
+clipboard.writeSync(`${r1},${g1},${b1}`);
+console.log(`
+new sRGB number had copy to your clipboard`);
